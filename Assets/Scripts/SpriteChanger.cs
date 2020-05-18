cb0495a45537a3807e686dafab5378fe9e3d7e7e
Raul Montes <montesraul7@gmail.com>
2020-05-18 12:21:21 -0700

    Began the sprite Mover for class
    Basically I set up folders to keep things organized for any future using. Also followed the class on 05/18/20 about the unity start of the interface.

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public SpriteRenderer renderSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderSprite = gameObject.GetComponent<SpriteRenderer>();
+        renderSprite.color = Color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
