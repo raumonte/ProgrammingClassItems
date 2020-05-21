6de9f52b6164f365d73809fe2f5b2ae4df364775
Raul Montes <montesraul7@gmail.com>
2020-05-21 11:29:35 -0700

    Added in new sprite and edited code
    Within it I edited the moving sprite code to go to the right. Also added it to two different sprites to different speeds.

---
+++ Assets/Scripts/MoveSpriteRight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSpriteRight : MonoBehaviour
+{
+    //This public float is to edit the numbers within the engine.
+    public float Speedyspeedboi = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*This part of the code tells the object which direction they should go. 
+         *While also taking in the framerate of the game and input of the developer to get the speed of the object.
+        */
+        transform.position += Vector3.right* Speedyspeedboi* Time.deltaTime;
+    }
+}
